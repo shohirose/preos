8eeabc72e83994870ee12fff8035ceee74ddfacd
shohirose <sho.hirose@gmail.com>
2016-05-11 23:14:50 -0500

    Add comments.

--- demo_pressbub.m
+++ demo_pressbub.m
@@ -1,5 +1,6 @@
-%% DEMO FOR PRESSBUB_PURECOMP_NEWTON
+%% Demo for pressbub_purecomp_newton()
 
+% The Definition of Variables
 % temp : temperature [K]
 % name : component's name
 % pressc : critical pressure
@@ -9,6 +10,7 @@
 % tol : toleratnce for convergence
 % maxiter : the maximum iteration
 
+% Input data
 temp   = 270; % [K]
 name = {'CH4', 'C2H6' 'C3H8' 'n-C4H10' 'n-C5H12' 'CO2'};
 pressc   = [4.600, 4.884, 4.246, 3.800, 3.374, 7.376]'*1e6; % [Pa]
@@ -16,7 +18,7 @@ tempc    = [190.6, 305.4, 369.8, 425.2, 469.6, 304.2]'; % [K]
 acentric = [0.008, 0.098, 0.152, 0.193, 0.251, 0.225]';
 ngas = size(pressc, 1);
 
-tol = 1e-6;
+tol = 1e-8;
 maxiter = 20;
 
 % Calculate bubble point pressure for each component.
@@ -26,8 +28,11 @@ for i = 1:ngas;
     if (temp >= tempc)
         error('Gas %s has no bubble point at the temperature %f.\\n', name(i), temp);
     end
+    % Calculate the initial estimate of bubble point pressure.
     pressb_int = pressbubest_purecomp(pressc(i), tempc(i), acentric(i), temp);
+    % Calculate bubble point pressure.
     pressb = pressbub_purecomp_newton(pressb_int, temp, pressc(i), tempc(i), acentric(i), tol, maxiter);
+    % Store the calculated value.
     pressb_calc = cat(1, pressb_calc, pressb);
 end
 
