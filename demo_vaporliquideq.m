cd6b622efacb0818ed755f00f5b5967aa4fe5fe7
shohirose <sho.hirose@gmail.com>
2016-05-15 01:02:03 -0500

    Create tpdss function.
    tpdss is a function to check if either one or two phase exist by using
    Tangent Plane Distance method with successive substitution.

--- demo_vaporliquideq.m
+++ demo_vaporliquideq.m
@@ -40,3 +40,29 @@ xlabel('Pressure [MPa]');
 ylabel('n_V, x_{CH_4}^V, x_{CH_4}^L');
 axis([-Inf,Inf,0,1]);
 legend('x_{CH_4}^V', 'x_{CH_4}^L', 'n_V','Orientation', 'horizontal');
+
+%% Demo for tpdss function
+
+press = pressdew*0.8;
+phasesplit = tpdss(comp_overall, press, temp, pressc, tempc, acentric, BIP);
+if phasesplit == true
+    fprintf('V and L phase exist at P = %1.3f\n', press*1e-6);
+else
+    fprintf('Either V or L phase coexists at P = %1.3f\n', press*1e-6);
+end
+
+press = (pressdew + pressbub)*0.5;
+phasesplit = tpdss(comp_overall, press, temp, pressc, tempc, acentric, BIP);
+if phasesplit == true
+    fprintf('V and L phase exist at P = %1.3f\n', press*1e-6);
+else
+    fprintf('Either V or L phase coexists at P = %1.3f\n', press*1e-6);
+end
+
+press = pressbub*1.2;
+phasesplit = tpdss(comp_overall, press, temp, pressc, tempc, acentric, BIP);
+if phasesplit == true
+    fprintf('V and L phase exist at P = %1.3f\n', press*1e-6);
+else
+    fprintf('Either V or L phase coexists at P = %1.3f\n', press*1e-6);
+end
---
+++ tpdss.m
@@ -0,0 +1,106 @@
+%% Tangend Plane Distance analysis by using successive substitution
+
+function twophase = tpdss(comp_overall, press, temp, pressc, tempc, acentric, BIP)
+
+% The initial estimate of equilibrium constant
+K = wilsoneq(press, temp, pressc, tempc, acentric);
+
+% Vapor-like phase
+comp_vap = compvapest(K, comp_overall);
+vaporphase = checkphasesplit(comp_vap, comp_overall, press, temp, pressc, tempc, acentric, BIP);
+
+% Liquid-like phase
+comp_liq = compliqest(K, comp_overall);
+liquidphase = checkphasesplit(comp_liq, comp_overall, press, temp, pressc, tempc, acentric, BIP);
+
+if (vaporphase == true) || (liquidphase == true)
+    twophase = true;
+else
+    twophase = false;
+end
+end
+
+function phasesplit = checkphasesplit(comp_check, comp_overall, press, temp, pressc, tempc, acentric, BIP)
+
+[fugcoef_overall, ~] = fugacitycoef_multicomp(comp_overall, press, temp, pressc, tempc, acentric, BIP);
+
+x = comp_check;
+tol = 1e-6;
+eps = 1;
+maxiter = 20;
+
+for iter = 1:maxiter
+    
+    xsum = sum(x);
+    %x = x/xsum;
+    
+    if max(abs(x - comp_overall)) < tol
+        phasesplit = false;
+        return;
+    end
+    
+    if eps < tol
+        break;
+    end
+    
+    [fugcoef_x, ~] = fugacitycoef_multicomp(x, press, temp, pressc, tempc, acentric, BIP);
+    eps = calcrsd(x, fugcoef_x, comp_overall, fugcoef_overall);
+    x = updatex(comp_overall, fugcoef_overall, fugcoef_x);
+    
+end
+
+if iter >= maxiter
+    fprintf('The iteration in checkphasesplit did not converge.\n');
+end
+
+if xsum > 1
+    phasesplit = true;
+else
+    phasesplit = false;
+end
+
+end
+
+function comp_vap = compvapest(K, comp_overall)
+
+ncomp = size(comp_overall,1);
+
+comp_vap = zeros(ncomp,1);
+for i = 1:ncomp
+    comp_vap(i) = K(i)*comp_overall(i);
+end
+
+end
+
+function comp_liq = compliqest(K, comp_overall)
+
+ncomp = size(comp_overall,1);
+
+comp_liq = zeros(ncomp,1);
+for i = 1:ncomp
+    comp_liq(i) = comp_overall(i)/K(i);
+end
+
+end
+
+function rsd = calcrsd(x, fugcoef_x, comp_overall, fugcoef_overall)
+
+ncomp = size(x,1);
+
+rsd = 0;
+for i = 1:ncomp
+    rsd = rsd + ( log(x(i)/comp_overall(i)*fugcoef_x(i)/fugcoef_overall(i)) )^2;
+end
+
+end
+
+function x = updatex(comp_overall, fugcoef_overall, fugcoef_x)
+
+ncomp = size(comp_overall,1);
+
+x = zeros(ncomp,1);
+for i = 1:ncomp
+    x(i) = comp_overall(i)*fugcoef_overall(i)/fugcoef_x(i);
+end
+
+end
