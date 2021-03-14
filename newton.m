38a0783911628581be50e7a6a6bf79720e24ce04
Sho Hirose <sho.hirose@gmail.com>
2021-03-14 21:06:36 +0900

    fix: Create newton function

---
+++ newton.m
@@ -0,0 +1,22 @@
+%% Compute the minimum of a given function using Newton-Raphson iteration.
+%
+% fun     : Scalar function to be minimized
+% grad    : Gradient function of 'fun'
+% hessian : Hessian function of 'fun'
+% x0      : Initial vector
+% tol     : Tolerance
+% maxiter : Maximum iteration
+function x = newton(fun, grad, hessian, x0, tol, maxiter)
+  x = x0;
+  eps = 1.0;
+  iter = 0;
+  while (eps > tol && iter < maxiter)
+    y = fun(x);
+    dfdx = grad(x);
+    H = hessian(x);
+    dx = -H\dfdx;
+    x = x + dx;
+    eps = norm(dx);
+    iter = iter + 1;
+  endwhile
+end
